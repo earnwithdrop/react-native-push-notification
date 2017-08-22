e5ec7f58a88a28579acf88080a9f327e3f95aa55
Daniel Whiffing <daniel@earnwithdrop.com>
2017-08-22 15:25:03 -0400

    Update ReactNativePushNotificationPackage.java

--- android/src/main/java/com/dieam/reactnativepushnotification/ReactNativePushNotificationPackage.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/ReactNativePushNotificationPackage.java
@@ -18,14 +18,7 @@ public class ReactNativePushNotificationPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 }
-
-
